f838c67822b0c4d66ded157035d90d2ac8b9fc2a
yaobus <yaobus@outlook.com>
2025-06-21 13:23:31 +0800

    update

--- ThinkITAM/DataBridge/DataBridge.cs
+++ ThinkITAM/DataBridge/DataBridge.cs
@@ -19,7 +19,7 @@ namespace ThinkITAM.DataBridge
         /// <summary>
         /// 版本号
         /// </summary>
-        public static string Version = "1.0.14";
+        public static string Version = "1.0.15";
 
 
         /// <summary>
