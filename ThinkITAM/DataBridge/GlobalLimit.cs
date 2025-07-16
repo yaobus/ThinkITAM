81c0b815c797527cd9fd351066d879e9e72b6ded
yaobus <yaobus@outlook.com>
2025-07-16 19:05:27 +0800

    update

--- ThinkITAM/DataBridge/GlobalLimit.cs
+++ ThinkITAM/DataBridge/GlobalLimit.cs
@@ -10,7 +10,7 @@
         /// <summary>
         /// IP地址数量限制，8192个
         /// </summary>
-        public static readonly int IpAddressCount = 8192;
+        public static readonly int IpAddressCount = 12288;
 
         /// <summary>
         ///  设备数量限制，1024个
@@ -18,18 +18,18 @@
         public static readonly int DevicesCount = 1024;
 
         /// <summary>
-        /// 机架数量限制，128个
+        /// 机架数量限制，256个
         /// </summary>
-        public static readonly int RackCount = 128;
+        public static readonly int RackCount = 256;
 
         /// <summary>
-        /// 终端数量限制，1024个
+        /// 终端数量限制，2048个
         /// </summary>
-        public static readonly int ComputerCount = 1024;
+        public static readonly int ComputerCount = 2048;
 
         /// <summary>
-        ///  节点数量限制，8192个
+        ///  节点数量限制，12288个
         /// </summary>
-        public static readonly int NodeCount = 8192;
+        public static readonly int NodeCount = 12288;
     }
 }
